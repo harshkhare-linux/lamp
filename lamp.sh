b7a1f28f08617fcbb8441d925fe6dc24173812cb
Harshkhare-linux <37015908+harshkhare-linux@users.noreply.github.com>
2020-07-04 13:21:41 +0530

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -7,7 +7,6 @@ sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 sudo apt-get install php7.3 php7.3-mysql php7.3-bcmath php7.3-gd php7.3-bz2 php7.3-xml php7.3-cli php7.3-common php7.3-intl php7.3-curl php7.3-json php7.3-zip php7.3-mbstring php7.3-mcrypt php7.3-soap
 sudo apt install mysql-server -y
-sudo apt install phpmyadmin -y
 sudo a2enmod rewrite ssl
 sudo service apache2 restart
 sudo apt install curl -y
